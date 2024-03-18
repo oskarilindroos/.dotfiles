5891f5654bfab70386bce84d6875ef01c4338608
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-03-18 21:59:08 +0200

    removed old dotfiles and added .zshrc

--- bin/layout-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-i3-msg "workspace 1 home; append_layout ~/.i3/workspace-1.json"
-urxvt -name "terminal" -e $SHELL -c 'screenfetch;$SHELL -i'&
-urxvt -name "cmatrix" -e $SHELL -c 'cmatrix;$SHELL -i'&
-urxvt -name "htop" -e $SHELL -c 'htop;$SHELL -i'&
-urxvt -name "ranger" -e $SHELL -c 'ranger;$SHELL -i'&
