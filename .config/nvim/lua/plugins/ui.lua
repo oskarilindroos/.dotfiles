bbb2988af7d3b24ad5ea94de1ad16a684d5a0679
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-03-27 18:08:04 +0200

    modularized the code more

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -9,6 +9,116 @@ return {
     end,
   },
 
+  -- Startup screen
+  {
+    'echasnovski/mini.starter',
+    opts = function()
+      local logo = table.concat({
+        '            ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z',
+        '            ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    ',
+        '            ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       ',
+        '            ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         ',
+        '            ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           ',
+        '            ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           ',
+      }, '\n')
+
+      local pad = string.rep(' ', 22)
+      local new_section = function(name, action, section)
+        return { name = name, action = action, section = pad .. section }
+      end
+
+      local starter = require 'mini.starter'
+  --stylua: ignore
+  local config = {
+    evaluate_single = true,
+    header = logo,
+    items = {
+      new_section("Find file",       "Telescope find_files",                                   "Telescope"),
+      new_section("Recent files",    "Telescope oldfiles",                                     "Telescope"),
+      new_section("Grep text",       "Telescope live_grep",                                    "Telescope"),
+      new_section("Config",          "Telescope find_files { cwd = vim.fn.stdpath 'config' }", "Config"),
+      new_section("Lazy",            "Lazy",                                                   "Config"),
+      new_section("New file",        "ene | startinsert",                                      "Built-in"),
+      new_section("Quit",            "qa",                                                     "Built-in"),
+      new_section("Session restore", [[lua require("persistence").load()]],                    "Session"),
+    },
+    footer = nil,
+    content_hooks = {
+      starter.gen_hook.adding_bullet(pad .. "░ ", false),
+      starter.gen_hook.aligning("center", "center"),
+    },
+  }
+      return config
+    end,
+  },
+
+  --  Indentation lines
+  {
+    'echasnovski/mini.indentscope',
+    opts = {
+      symbol = '│',
+      options = { try_as_border = true },
+    },
+  },
+
+  -- File browser
+  {
+    'echasnovski/mini.files',
+    opts = {
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_preview = 30,
+      },
+    },
+    vim.keymap.set('n', '<leader>f', ':lua MiniFiles.open()<CR>', { desc = 'File explorer' }),
+  },
+
+  -- Statusline
+  {
+    'echasnovski/mini.statusline',
+    -- opts = {
+    --   use_icons = vim.g.have_nerd_font,
+    -- },
+    config = function()
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+
+  -- Better options menu
+  {
+    'stevearc/dressing.nvim',
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.input(...)
+      end
+    end,
+  },
+
   -- Centered layout
   {
     'shortcuts/no-neck-pain.nvim',
@@ -36,11 +146,5 @@ return {
         ['<leader>u'] = { name = '[u]i', _ = 'which_key_ignore' },
       }
     end,
-  },
-
-  -- Telescope file explorer
-  {
-    'nvim-telescope/telescope-file-browser.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   },
 }
