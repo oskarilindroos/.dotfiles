34f7fe6fffcd05a262e377bb32962c08bf88e8cb
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-07 15:04:24 +0300

    reduced padding, increased default font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,9 +2,17 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font("GeistMono Nerd Font")
+config.font_size = 14.0
 config.color_scheme = "Bitmute"
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
+}
 
 config.keys = {
 	{
