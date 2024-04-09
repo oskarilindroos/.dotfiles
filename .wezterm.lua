e81dc99071420fc4349f920a9a0cf50bb5322c7e
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-09 15:57:07 +0300

    added commented out light theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,10 @@ local config = {}
 config.font = wezterm.font("GeistMono Nerd Font")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 14.0
+
+-- Color scheme
 config.color_scheme = "Bitmute"
+-- config.color_scheme = "Alabaster"
 
 config.hide_tab_bar_if_only_one_tab = true
 
