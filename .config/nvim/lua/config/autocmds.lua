ddba51287ac945e6866e89f40ddd945dc42e8e61
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-12 14:20:43 +0300

    added autocmds for different line numbering in insert and normal mode

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,28 @@
--- Highlight when yanking (copying) text
+-- Highlight when yanking text
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Relative line numbers in normal mode
+vim.api.nvim_create_autocmd('InsertLeave', {
+  desc = 'Relative line numbers in normal mode',
+  group = vim.api.nvim_create_augroup('relative-line-numbers', { clear = true }),
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+  end,
+})
+
+-- Absolute line numbers in insert mode
+vim.api.nvim_create_autocmd('InsertEnter', {
+  desc = 'Absolute line numbers in insert mode',
+  group = vim.api.nvim_create_augroup('absolute-line-numbers', { clear = true }),
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = false
   end,
 })
