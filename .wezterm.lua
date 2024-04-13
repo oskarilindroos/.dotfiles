3be347df921978752e3e784303616b62ff35b9c9
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-13 13:26:28 +0300

    changed terminal font to Fixedsys Core

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 -- Font settings
-config.font = wezterm.font("Terminess Nerd Font")
+config.font = wezterm.font("Fixedsys Core")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 14.0
 
