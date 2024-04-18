7db49a7fe1a8d4dc7e3977c9fceffec419fa82a3
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-18 11:05:44 +0300

    removed unnecessary keybinds

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,11 +10,6 @@ vim.keymap.set('n', '<C-Up>', ':resize +4<CR>')
 vim.keymap.set('n', '<C-Down>', ':resize -4<CR>')
 vim.keymap.set('n', '<C-Left>', ':vertical resize +4<CR>')
 vim.keymap.set('n', '<C-Right>', ':vertical resize -4<CR>')
-vim.keymap.set('n', '<leader>we', '<C-w>=', { desc = 'Resize windows equally' })
-
--- Splits
-vim.keymap.set('n', '<leader>-', ':split<CR>', { desc = 'Horizontal split' })
-vim.keymap.set('n', '<leader>|', ':vsplit<CR>', { desc = 'Vertical split' })
 
 -- Dark/light background toggle
 vim.keymap.set('n', '<leader>ub', ':exec &bg=="light"? "set bg=dark" : "set bg=light"<CR>', { desc = 'Toggle dark/light mode' })
@@ -22,20 +17,11 @@ vim.keymap.set('n', '<leader>ub', ':exec &bg=="light"? "set bg=dark" : "set bg=l
 -- Linewrapping toggle
 vim.keymap.set('n', '<leader>uw', ':set wrap!<CR>', { desc = 'Toggle line wrapping' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Use CTRL+<hjkl> to switch between windows
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Show diagnostic floating window (for really long messages)
+-- Show diagnostic floating window (for really long diagnostics)
 vim.keymap.set('n', '<leader>df', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = 'Show diagnostic floating window' })
