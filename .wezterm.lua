b28c50d3992fc65b373e0b80c5791c456cd69411
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-05-15 10:48:26 +0300

    changed font to GoMono Nerd Font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 -- Font settings
-config.font = wezterm.font("Go Mono")
+config.font = wezterm.font("GoMono Nerd Font")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 14.0
 
